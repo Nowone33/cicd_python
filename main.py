33c4a8c58f69fdfda858944dcdc23994a5a3e794
Nowone33 <lamtom3301@gmail.com>
2026-05-19 19:23:16 +0200

    fix - main py error

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     print("=== Application de Calculs ===")
     print(f"2 + 3 = {additionner(2, 3)}")
     print(f"10 - 4 = {soustraire(10, 4)}")
-    print(f"10 - 4 = {soustraire(1, 4)}")
+    print(f"10 - 4 = {soustraire(10, 4)}")
     print(f"3 × 4 = {multiplier(3, 4)}")
 
 
