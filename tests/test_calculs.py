e6180691eba083222ee6be9bbe272dd614f0b6b5
Nowone33 <lamtom3301@gmail.com>
2026-05-19 22:48:18 +0200

    fix -  job test add test for coverage

--- tests/test_calculs.py
+++ tests/test_calculs.py
@@ -11,10 +11,22 @@ class TestAdditionner:
     def test_flottants(self):
         assert additionner(0.1, 0.2) == pytest.approx(0.3, rel=1e-7)
 
+    def test_failed_notNumber(self):
+        with pytest.raises( TypeError): 
+            additionner("pas un nombre", 0.2)
+
 
 class TestSoustraire:
     def test_resultat(self):
         assert soustraire(10, 4) == 6
+
+    def test_failed_notNumber(self):
+        with pytest.raises( TypeError): 
+            soustraire("pas un nombre", 0.2)
+
+    def test_failed_negativ_result(self):
+        with pytest.raises(ArithmeticError):
+            soustraire(5,20)
 
 
 class TestMultiplier:
@@ -25,6 +37,19 @@ class TestMultiplier:
         with pytest.raises(TypeError):
             multiplier("5", 2)
 
+
+class TestDiviser:
+    def test_entiers(self);
+        assert diviser(20,5) == 4
+
+    def test_type_incorrect(self):
+        with pytest.raises(TypeError):
+            diviser("5", 2)
+
+    def test_failed_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            diviser(20,0)
+    
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
